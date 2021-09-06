4f5387e199897748e5cafb3f147d6443f9f99b72
DxrkxDev <xdxrk12@gmail.com>
2021-09-06 02:10:40 -0500

    comming on index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('bot ONLINE!');
+});
+
+
+
+client.login('ODgyNDM1ODAyMDkxNjI2NTE3.YS7WYg.qrCkkocjClLkCqcfVL57lEgqEdw');
